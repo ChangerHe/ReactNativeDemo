c090ae60cb0200cb598967a7ecb500fbf267c6e0
ChangerHe <244737162@qq.com>
2018-06-06 19:27:29 +0800

    feat: 统一双端显示样式

--- pages/Page1.js
+++ pages/Page1.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Platform,
     StyleSheet,
@@ -7,75 +7,92 @@ import {
     View,
     FlatList,
     TouchableOpacity,
-		SafeAreaView
+    SafeAreaView,
+    Alert
 } from 'react-native';
-
-
 
 export default class Cinemas extends Component {
     static navigationOptions = {
-        title: '我的电影',
+        title: '我的电影'
     };
     render() {
-        const {state,goBack}=this.props.navigation;
+        const {state, goBack} = this.props.navigation;
         return (
-					<SafeAreaView  style={styles.container}>
-            <View  style={styles.parent}>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.parent}>
 
-                    <Text style={[styles.childfirst,styles.margin]}> View1 </Text>
-                    <Text style={[styles.childsecond,styles.margin]}> View2 </Text>
-                    <Text style={[styles.childthird,styles.margin]}> View3 </Text>
-                    <Text style={styles.childthird}> View3 </Text>
-                    <Text style={styles.childthird}> View3 </Text>
-                    <Text style={styles.childthird}> View3 </Text>
+                    <Text style={[styles.childfirst, styles.margin]}>
+                        View1
+                    </Text>
+                    <Text style={[styles.childsecond, styles.margin]}>
+                        View2
+                    </Text>
+                    <Text style={[styles.childthird, styles.margin]}>
+                        View3
+                    </Text>
+                    <Text style={styles.childthird}>
+                        View3
+                    </Text>
+                    <Text style={styles.childthird}>
+                        View3
+                    </Text>
+                    <Text style={styles.childthird}>
+                        View3
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => {
+                            Alert.alert('TouchableOpacity')
+                        }}
+                    >
+                        <Text>
+                            touchable view
+                        </Text>
+                    </TouchableOpacity>
 
-            </View>
-					
-					</SafeAreaView>
+                </View>
+
+            </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-		container: {
+    container: {
         flex: 0,
-				width: '100%',
-				height: '100%',
+        width: '100%',
+        height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#e5fffd',
+        backgroundColor: '#e5fffd'
     },
-    parent:{
-        flex:1,
-        flexDirection:'row',
-        opacity:0.4,
-
-        flexWrap:'wrap',//换行
-        justifyContent:'space-around',
+    parent: {
+        flex: 1,
+        flexDirection: 'row',
+        opacity: 0.4,
+        padding: 30,
+        flexWrap: 'wrap', //换行
+        justifyContent: 'space-around'
     },
-    childfirst:{
-        backgroundColor:'#676677',
-        width:100,
-        height:100,
-        fontSize:13,
-
+    childfirst: {
+        backgroundColor: '#676677',
+        width: 100,
+        height: 100,
+        fontSize: 13
     },
-    childsecond:{
-        backgroundColor:'#9fbb74',
-        width:100,
-        height:100,
-        fontSize:13,
-
+    childsecond: {
+        backgroundColor: '#9fbb74',
+        width: 100,
+        height: 100,
+        fontSize: 13
     },
-    childthird:{
-        backgroundColor:'#bb7478',
-        width:100,
-        height:100,
-        fontSize:13,
-
+    childthird: {
+        backgroundColor: '#bb7478',
+        width: 100,
+        height: 100,
+        fontSize: 13
     },
-    margin:{
+    margin: {
 
-        marginBottom:10,
+        marginBottom: 10
     }
 });
