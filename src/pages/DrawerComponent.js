a6461681518043f57e3d10a85edb41a0671eddb1
ChangerHe <244737162@qq.com>
2018-09-04 11:59:04 +0800

    test: push test

--- src/pages/DrawerComponent.js
+++ src/pages/DrawerComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 
+console.log(1)
 export default class Page4 extends React.Component {
     render() {
         const { navigation } = this.props;
